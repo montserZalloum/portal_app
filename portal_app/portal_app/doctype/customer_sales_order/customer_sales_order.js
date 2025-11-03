9a8b5a3b737e31f520dc71def479606ad63d4493
montserZalloum <zmontsaer@gmail.com>
2025-11-03 07:05:12 +0000

    Enhance Customer Sales Order functionality by adding a check for 'corex_system_sales_order_id' before displaying the 'Convert to Sales Order' button. Store the created sales order ID upon successful creation and update JSON configuration to include the new field. Adjust CSS to hide the discard button for specific paths.

--- portal_app/portal_app/doctype/customer_sales_order/customer_sales_order.js
+++ portal_app/portal_app/doctype/customer_sales_order/customer_sales_order.js
@@ -3,7 +3,8 @@
 
 frappe.ui.form.on("Customer Sales Order", {
 	refresh(frm) {
-		if (frm.doc.docstatus === 1 && cur_frm.doc.corex_customer_quotation_id && cur_frm.doc.corex_customer_quotation_id != '') {
+		if (frm.doc.docstatus === 1 && frm.doc.corex_customer_quotation_id && frm.doc.corex_customer_quotation_id != '' 
+			&& frm.doc.corex_system_sales_order_id && frm.doc.corex_system_sales_order_id != '') {
 			frm.add_custom_button("Convert to Sales Order", function() {
 				frappe.call({
 					method: "portal_app.portal_app.doctype.customer_sales_order.customer_sales_order.create_sales_order",
@@ -12,7 +13,12 @@ frappe.ui.form.on("Customer Sales Order", {
 					},
 					callback: function(r) {
 						if (!r.exc) {
-							
+							// Store the created sales order ID
+							if (r.message && r.message.name) {
+								frm.set_value("corex_system_sales_order_id", r.message.name);
+								frm.save();
+							}
+
 							frappe.msgprint({
 								title: "Success",
 								message: "Sales Order created successfully",
